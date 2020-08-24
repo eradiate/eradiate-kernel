05e520c41474b1aa039c5c589f4cf6ad740ece3d
schunkes <sebastian.schunke@rayference.eu>
2020-08-24 11:02:41 +0200

    Added bilambertian BSDF plugin

---
+++ src/bsdfs/bilambertian.cpp
@@ -0,0 +1,199 @@
+#include <mitsuba/core/properties.h>
+#include <mitsuba/core/spectrum.h>
+#include <mitsuba/core/warp.h>
+#include <mitsuba/render/bsdf.h>
+#include <mitsuba/render/fresnel.h>
+#include <mitsuba/render/texture.h>
+
+NAMESPACE_BEGIN(mitsuba)
+
+/**!
+
+.. _bsdf-bilambertian:
+
+Bi-lambertian material (:monosp:`bilambertian`)
+-----------------------------------------------
+
+.. pluginparameters::
+
+* - reflectance
+  - |spectrum|
+  - Specifies the diffuse albedo of the material (Default: 0.5)
+* - transmittance
+  - |spectrum|
+  - Specifies the diffuse transmittance of the material (Default: 0.5)
+
+The bilambertian material represents a material that scatters light diffusely
+into the entire sphere. The reflectance specifies the amount of light scattered
+into the hemisphere that contains the incoming direction, while the
+transmittance specifies the amount of light scattered into the opposite
+hemisphere.
+
+*/
+
+template <typename Float, typename Spectrum>
+class BiLambertian final : public BSDF<Float, Spectrum> {
+public:
+    MTS_IMPORT_BASE(BSDF, m_flags, m_components)
+    MTS_IMPORT_TYPES(Texture)
+
+    BiLambertian(const Properties &props) : Base(props) {
+        m_reflectance   = props.texture<Texture>("reflectance", .5f);
+        m_transmittance = props.texture<Texture>("transmittance", .5f);
+
+        m_components.push_back(BSDFFlags::DiffuseReflection |
+                               BSDFFlags::FrontSide | BSDFFlags::BackSide);
+        m_components.push_back(BSDFFlags::DiffuseTransmission |
+                               BSDFFlags::FrontSide | BSDFFlags::BackSide);
+
+        m_flags = m_components[0] | m_components[1];
+    }
+
+    std::pair<BSDFSample3f, Spectrum>
+    sample(const BSDFContext &ctx, const SurfaceInteraction3f &si,
+           Float sample1, const Point2f &sample2, Mask active) const override {
+        MTS_MASKED_FUNCTION(ProfilerPhase::BSDFSample, active);
+
+        bool has_reflect  = ctx.is_enabled(BSDFFlags::DiffuseReflection, 0),
+             has_transmit = ctx.is_enabled(BSDFFlags::DiffuseTransmission, 1);
+
+        if (unlikely((!has_reflect && !has_transmit) || none_or<false>(active)))
+            return { zero<BSDFSample3f>(), UnpolarizedSpectrum(0.f) };
+
+        Float cos_theta_i = Frame3f::cos_theta(si.wi);
+        Vector3f wo       = warp::square_to_cosine_hemisphere(sample2);
+        Float cos_theta_o = Frame3f::cos_theta(wo);
+
+        BSDFSample3f bs   = zero<BSDFSample3f>();
+        UnpolarizedSpectrum value(0.f);
+
+        // Flip the outgoing direction if the incoming comes from "behind"
+        wo = select(cos_theta_i > 0, wo, Vector3f(wo.x(), wo.y(), -wo.z()));
+
+        // Select the lobe to be sampled
+        UnpolarizedSpectrum r            = m_reflectance->eval(si, active),
+                            t            = m_transmittance->eval(si, active);
+        Float reflection_sampling_weight = hmean(r / (r + t));
+        // Handle case where r = t = 0
+        masked(reflection_sampling_weight, isnan(reflection_sampling_weight)) = 0.f;
+        
+        Mask selected_r = (sample1 < reflection_sampling_weight) && active,
+             selected_t = (sample1 >= reflection_sampling_weight) && active;
+
+        // Evaluate
+        value = select(active, cos_theta_o, 0.f);
+        value[selected_r] *= r;
+        value[selected_t] *= t;
+
+        // Compute PDF
+        bs.pdf = select(active, warp::square_to_cosine_hemisphere_pdf(wo), 0.f);
+        bs.pdf = select(selected_r, bs.pdf * reflection_sampling_weight, bs.pdf);
+        bs.pdf = select(selected_t, bs.pdf * (1.f - reflection_sampling_weight), 
+                        bs.pdf);
+
+        // Set other interaction fields
+        bs.eta               = 1.f;
+        bs.sampled_component = select(selected_r, UInt32(0), UInt32(1));
+        bs.sampled_type =
+            select(selected_r, UInt32(+BSDFFlags::DiffuseReflection),
+                   UInt32(+BSDFFlags::DiffuseTransmission));
+
+        // Flip the outgoing direction if transmission was selected
+        bs.wo = select(selected_r, wo, Vector3f(wo.x(), wo.y(), -wo.z()));
+
+        return { bs, select(active && bs.pdf > 0.f,
+                            unpolarized<Spectrum>(value), 0.f) };
+    }
+
+    Spectrum eval(const BSDFContext &ctx, const SurfaceInteraction3f &si,
+                  const Vector3f &wo, Mask active) const override {
+        MTS_MASKED_FUNCTION(ProfilerPhase::BSDFEvaluate, active);
+
+        bool has_reflect  = ctx.is_enabled(BSDFFlags::DiffuseReflection, 0),
+             has_transmit = ctx.is_enabled(BSDFFlags::DiffuseTransmission, 1);
+
+        if (unlikely((!has_reflect && !has_transmit) || none_or<false>(active)))
+            return 0.f;
+
+        Float cos_theta_i = Frame3f::cos_theta(si.wi),
+              cos_theta_o = Frame3f::cos_theta(wo);
+
+        UnpolarizedSpectrum result(0.f);
+
+        if (has_reflect) {
+            Mask is_reflect =
+                Mask(sign(cos_theta_i) == sign(cos_theta_o)) && active;
+            result[is_reflect] = m_reflectance->eval(si, is_reflect);
+        }
+
+        if (has_transmit) {
+            Mask is_transmit =
+                Mask(sign(cos_theta_i) != sign(cos_theta_o)) && active;
+            result[is_transmit] = m_transmittance->eval(si, is_transmit);
+        }
+
+        result[active] *= math::InvPi<Float> * abs(cos_theta_o);
+        return select(active, result, 0.f);
+    }
+
+    Float pdf(const BSDFContext &ctx, const SurfaceInteraction3f &si,
+              const Vector3f &wo, Mask active) const override {
+        MTS_MASKED_FUNCTION(ProfilerPhase::BSDFEvaluate, active);
+
+        bool has_reflect  = ctx.is_enabled(BSDFFlags::DiffuseReflection, 0),
+             has_transmit = ctx.is_enabled(BSDFFlags::DiffuseTransmission, 1);
+
+        if (unlikely((!has_reflect && !has_transmit) || none_or<false>(active)))
+            return 0.f;
+
+        Float cos_theta_i = Frame3f::cos_theta(si.wi),
+              cos_theta_o = Frame3f::cos_theta(wo);
+        Vector3f wo_flip{ wo.x(), wo.y(), abs(cos_theta_o) };
+        Float result =
+            select(active, warp::square_to_cosine_hemisphere_pdf(wo_flip), 0.f);
+
+        UnpolarizedSpectrum r            = m_reflectance->eval(si, active),
+                            t            = m_transmittance->eval(si, active);
+        Float reflection_sampling_weight = hmean(r / (r + t));
+        // Handle case where r = t = 0
+        masked(reflection_sampling_weight, isnan(reflection_sampling_weight)) = 0.f;
+        
+        if (has_reflect) {
+            Mask is_reflect =
+                Mask(sign(cos_theta_i) == sign(cos_theta_o)) && active;
+            masked(result, is_reflect) *= reflection_sampling_weight;
+        }
+
+        if (has_transmit) {
+            Mask is_transmit =
+                Mask(sign(cos_theta_i) != sign(cos_theta_o)) && active;
+            masked(result, is_transmit) *= (1.f - reflection_sampling_weight);
+        }
+
+        return result;
+    }
+
+    void traverse(TraversalCallback *callback) override {
+        callback->put_object("reflectance", m_reflectance.get());
+        callback->put_object("transmittance", m_transmittance.get());
+    }
+
+    std::string to_string() const override {
+        std::ostringstream oss;
+        oss << "Bilambertian[" << std::endl
+            << "  reflectance = " << string::indent(m_reflectance) << std::endl
+            << "  transmittance = " << string::indent(m_transmittance)
+            << std::endl
+            << "]";
+        return oss.str();
+    }
+
+    MTS_DECLARE_CLASS()
+private:
+    ref<Texture> m_reflectance;
+    ref<Texture> m_transmittance;
+};
+
+MTS_IMPLEMENT_CLASS_VARIANT(BiLambertian, BSDF)
+MTS_EXPORT_PLUGIN(BiLambertian, "Bi-Lambertian material")
+NAMESPACE_END(mitsuba)
---
+++ src/bsdfs/tests/test_bilambertian.py
@@ -0,0 +1,89 @@
+import mitsuba
+import pytest
+import enoki as ek
+import numpy as np
+
+
+def test_instantiation(variant_scalar_rgb):
+    from mitsuba.render import BSDFFlags
+    from mitsuba.core.xml import load_dict
+
+    b = load_dict({"type": "bilambertian"})
+    assert b is not None
+    assert b.component_count() == 2
+    expected_flags_reflection = \
+        BSDFFlags.DiffuseReflection | BSDFFlags.FrontSide | BSDFFlags.BackSide
+    expected_flags_transmission = \
+        BSDFFlags.DiffuseTransmission | BSDFFlags.FrontSide | BSDFFlags.BackSide
+    assert b.flags(0) == expected_flags_reflection
+    assert b.flags(1) == expected_flags_transmission
+
+
+def test_eval_pdf(variant_scalar_rgb):
+    from mitsuba.core.xml import load_dict
+    from mitsuba.render import BSDFContext, SurfaceInteraction3f
+    from mitsuba.core import Frame3f
+
+    for (r, t) in [(0.2, 0.4), (0.9, 0.1)]:
+        albedo = r + t
+
+        bsdf = load_dict({
+            "type": "bilambertian",
+            "reflectance": r,
+            "transmittance": t
+        })
+
+        ctx = BSDFContext()
+
+        si = SurfaceInteraction3f()
+        si.p = [0, 0, 0]
+        si.n = [0, 0, 1]
+
+        for wi in [[0, 0, 1], [0, 0, -1]]:  # We try from both the front and back sides
+            si.wi = wi
+            si.sh_frame = Frame3f(si.n)
+
+            for i in range(20):
+                theta = i / 19.0 * ek.pi  # We cover the entire circle
+
+                wo = [ek.sin(theta), 0, ek.cos(theta)]
+                v_pdf = bsdf.pdf(ctx, si, wo=wo)
+                v_eval = bsdf.eval(ctx, si, wo=wo)
+
+                if ek.dot(wi, wo) > 0:
+                    # reflection
+                    assert ek.allclose(v_eval, r * ek.abs(wo[2]) / ek.pi)
+                    assert ek.allclose(v_pdf, r / albedo *
+                                       ek.abs(wo[2]) / ek.pi)
+                else:
+                    # transmission
+                    assert ek.allclose(v_eval, t * ek.abs(wo[2]) / ek.pi)
+                    assert ek.allclose(v_pdf, t / albedo *
+                                       ek.abs(wo[2]) / ek.pi)
+
+
+@pytest.mark.parametrize("r,t", [
+    [0.6, 0.2],
+    [0.2, 0.6],
+    # [1.0, 0.0],  # Fails inexplicably
+    [0.0, 1.0],
+    # [0.0, 0.0]   # Fails inexplicably
+])
+def test_chi2(variant_packet_rgb, r, t):
+    from mitsuba.python.chi2 import BSDFAdapter, ChiSquareTest, SphericalDomain
+
+    xml = f"""
+        <spectrum name="reflectance" value="{r}"/>
+        <spectrum name="transmittance" value="{t}"/>
+    """
+
+    sample_func, pdf_func = BSDFAdapter("bilambertian", xml)
+
+    chi2 = ChiSquareTest(
+        domain=SphericalDomain(),
+        sample_func=sample_func,
+        pdf_func=pdf_func,
+        sample_dim=3
+    )
+
+    assert chi2.run()
