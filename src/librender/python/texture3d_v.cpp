3a3f2d6747418d98a103d27860c3145e80cb05de
Vincent Leroy <vincent.leroy@rayference.eu>
2020-09-21 15:42:29 +0200

    Added missing Python bindings for Volume API

--- src/librender/python/texture3d_v.cpp
+++ src/librender/python/texture3d_v.cpp
@@ -1,33 +1,29 @@
-#include <mitsuba/core/properties.h>
 #include <mitsuba/render/interaction.h>
-#include <mitsuba/render/spectrum.h>
+#include <mitsuba/render/texture.h>
 #include <mitsuba/python/python.h>
 
 MTS_PY_EXPORT(Volume) {
+    MTS_PY_IMPORT_TYPES(Volume)
     MTS_PY_CLASS(Volume, Object)
-        .def(py::init<const Properties &>())
         .def("eval",
-            vectorize(py::overload_cast<const Interaction3f&, Mask>(
-                &Volume::eval, py::const_)),
+            vectorize(&Volume::eval),
             "it"_a, "active"_a = true, D(Volume, eval))
         .def("eval_1",
-            vectorize(py::overload_cast<const Interaction3f&, Mask>(
-                &Volume::eval_1, py::const_)),
+            vectorize(&Volume::eval_1),
             "it"_a, "active"_a = true, D(Volume, eval_1))
         .def("eval_3",
-            vectorize(py::overload_cast<const Interaction3f&, Mask>(
-                &Volume::eval_3, py::const_)),
+            vectorize(&Volume::eval_3),
             "it"_a, "active"_a = true, D(Volume, eval_3))
         .def("eval_gradient",
-             vectorize(py::overload_cast<const Interaction3f&, Mask>(&Volume::eval_gradient,
-                                                            py::const_),
-             D(Volume, eval_gradient), "it"_a, "active"_a = true)
-        .def("eval_gradient",
-             vectorize_wrapper(py::overload_cast<const Interaction3fP &, MaskP>(
-                 &Volume::eval_gradient, py::const_)),
-             D(Volume, eval_gradient), "it"_a, "active"_a = true)
-        .def_method(Volume, max)
-        .def_method(Volume, bbox)
-        .def_method(Volume, resolution)
-        .def("__repr__", &Volume::to_string);
+            vectorize(&Volume::eval_gradient),
+            "it"_a, "active"_a = true, D(Volume, eval_gradient))
+        .def("max",
+            &Volume::max,
+            D(Volume, max))
+        .def("bbox",
+            &Volume::bbox,
+            D(Volume, bbox))
+        .def("resolution",
+            &Volume::resolution,
+            D(Volume, resolution));
 }
